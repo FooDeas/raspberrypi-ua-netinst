8fe53df5d95a480b9f3e81d41f518e079965ffbe
FooDeas <foodeas@aeberlein.de>
2017-04-07 18:00:13 +0200

    installer: fix possibly missing apt-transport-https (#68)

--- scripts/opt/raspberrypi-ua-netinst/install.sh
+++ scripts/opt/raspberrypi-ua-netinst/install.sh
@@ -712,6 +712,9 @@ if [ -z "${cdebootstrap_cmdline}" ]; then
 	if [ "${hwrng_support}" = "1" ]; then
 		base_packages="${base_packages},rng-tools"
 	fi
+	if [ "$(find /rootfs/boot/raspberrypi-ua-netinst/config/apt/ -maxdepth 1 -type f -name "*.list" | wc -l)" != 0 ]; then
+		base_packages="${base_packages},apt-transport-https"
+	fi
 	
 	# minimal
 	minimal_packages="ifupdown,net-tools,openssh-server,dosfstools"
