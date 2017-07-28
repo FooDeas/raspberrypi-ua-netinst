d01fd04af1816261d7a03d805fdce244599226cb
FooDeas <foodeas@aeberlein.de>
2017-07-28 12:39:33 +0200

    installer: code formatting

--- scripts/opt/raspberrypi-ua-netinst/install.sh
+++ scripts/opt/raspberrypi-ua-netinst/install.sh
@@ -168,39 +168,39 @@ led_sos() {
 	local led_on=1
 	local led_off=0
 
-# Setting leds on and off works the other way round on Pi Zero and Pi Zero W
-# Also led0 (the only led on the Zeros) is the activity led
-if [ ${rpi_hardware_version:0:4} = "Zero" ]; then
-	led_on=0
-	led_off=1
-fi
+	# Setting leds on and off works the other way round on Pi Zero and Pi Zero W
+	# Also led0 (the only led on the Zeros) is the activity led
+	if [ ${rpi_hardware_version:0:4} = "Zero" ]; then
+		led_on=0
+		led_off=1
+	fi
 
 	if [ -e /sys/class/leds/led0 ]; then (echo none > /sys/class/leds/led0/trigger) &> /dev/null; else led0=; fi
 	if [ -e /sys/class/leds/led1 ]; then (echo none > /sys/class/leds/led1/trigger) &> /dev/null; else led1=; fi
 	for i in $(seq 1 3); do
-		if [ -n "$led0" ]; then (echo $led_on > "${led0}"/brightness) &> /dev/null; fi
-		if [ -n "$led1" ]; then (echo $led_on > "${led1}"/brightness) &> /dev/null; fi
+		if [ -n "$led0" ]; then (echo ${led_on} > "${led0}"/brightness) &> /dev/null; fi
+		if [ -n "$led1" ]; then (echo ${led_on} > "${led1}"/brightness) &> /dev/null; fi
 		sleep 0.3s;
-		if [ -n "$led0" ]; then (echo $led_off > "${led0}"/brightness) &> /dev/null; fi
-		if [ -n "$led1" ]; then (echo $led_off > "${led1}"/brightness) &> /dev/null; fi
+		if [ -n "$led0" ]; then (echo ${led_off} > "${led0}"/brightness) &> /dev/null; fi
+		if [ -n "$led1" ]; then (echo ${led_off} > "${led1}"/brightness) &> /dev/null; fi
 		sleep 0.2s;
 	done
 	sleep 0.1s;
 	for i in $(seq 1 3); do
-		if [ -n "$led0" ]; then (echo $led_on > "${led0}"/brightness) &> /dev/null; fi
-		if [ -n "$led1" ]; then (echo $led_on > "${led1}"/brightness) &> /dev/null; fi
+		if [ -n "$led0" ]; then (echo ${led_on} > "${led0}"/brightness) &> /dev/null; fi
+		if [ -n "$led1" ]; then (echo ${led_on} > "${led1}"/brightness) &> /dev/null; fi
 		sleep 0.8s;
-		if [ -n "$led0" ]; then (echo $led_off > "${led0}"/brightness) &> /dev/null; fi
-		if [ -n "$led1" ]; then (echo $led_off > "${led1}"/brightness) &> /dev/null; fi
+		if [ -n "$led0" ]; then (echo ${led_off} > "${led0}"/brightness) &> /dev/null; fi
+		if [ -n "$led1" ]; then (echo ${led_off} > "${led1}"/brightness) &> /dev/null; fi
 		sleep 0.2s;
 	done
 	sleep 0.1s;
 	for i in $(seq 1 3); do
-		if [ -n "$led0" ]; then (echo $led_on > "${led0}"/brightness) &> /dev/null; fi
-		if [ -n "$led1" ]; then (echo $led_on > "${led1}"/brightness) &> /dev/null; fi
+		if [ -n "$led0" ]; then (echo ${led_on} > "${led0}"/brightness) &> /dev/null; fi
+		if [ -n "$led1" ]; then (echo ${led_on} > "${led1}"/brightness) &> /dev/null; fi
 		sleep 0.3s;
-		if [ -n "$led0" ]; then (echo $led_off > "${led0}"/brightness) &> /dev/null; fi
-		if [ -n "$led1" ]; then (echo $led_off > "${led1}"/brightness) &> /dev/null; fi
+		if [ -n "$led0" ]; then (echo ${led_off} > "${led0}"/brightness) &> /dev/null; fi
+		if [ -n "$led1" ]; then (echo ${led_off} > "${led1}"/brightness) &> /dev/null; fi
 		sleep 0.2s;
 	done
 	sleep 1.5s;
@@ -611,8 +611,6 @@ case "${rpi_hardware}" in
 	"a32082") rpi_hardware_version="3 Model B" ;;
 	*) rpi_hardware_version="unknown (${rpi_hardware})" ;;
 esac
-
-
 
 echo
 echo "=================================================="
