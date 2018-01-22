b6cd12dcd12040d47c530fde502fd4b42fc51521
zejn <zejn@users.noreply.github.com>
2018-01-22 15:26:42 +0100

    installer: force FAT32 to disable mkfs.vfat guessing the FAT version to use (#130, #127)

--- scripts/opt/raspberrypi-ua-netinst/install.sh
+++ scripts/opt/raspberrypi-ua-netinst/install.sh
@@ -1496,9 +1496,9 @@ mdev -s
 
 echo -n "Initializing /boot as vfat... "
 if [ -z "${boot_volume_label}" ]; then
-	mkfs.vfat -s 1 "${bootpartition}" &> /dev/null || fail
+	mkfs.vfat -F 32 -s 1 "${bootpartition}" &> /dev/null || fail
 else
-	mkfs.vfat -s 1 -n "${boot_volume_label}" "${bootpartition}" &> /dev/null || fail
+	mkfs.vfat -F 32 -s 1 -n "${boot_volume_label}" "${bootpartition}" &> /dev/null || fail
 fi
 echo "OK"
 
